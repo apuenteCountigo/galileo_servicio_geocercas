ef158fa505011e848189288a4b58b33efe2a9c81
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-09-05 05:31:20 -0400

    UPDATE MODELO BALIZA

--- src/main/java/com/galileo/cu/geocercas/ServicioGeocercasApplication.java
+++ src/main/java/com/galileo/cu/geocercas/ServicioGeocercasApplication.java
@@ -4,14 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.boot.CommandLineRunner;
 
 @EnableFeignClients
 @EnableEurekaClient
 @SpringBootApplication
-public class ServicioGeocercasApplication {
+public class ServicioGeocercasApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServicioGeocercasApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("**************************************");
+		System.out.println("GEOCERCAS V-2409050530");
 	}
 
 }
