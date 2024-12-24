2d8e3a445fe1b086de44cb2fae1d519f44ae4160
apuentes+1-at-178934116267 <apuente@soaint.com>
2024-12-24 13:34:27 -0500

    V1.1.4

--- src/main/java/com/galileo/cu/geocercas/ServicioGeocercasApplication.java
+++ src/main/java/com/galileo/cu/geocercas/ServicioGeocercasApplication.java
@@ -18,7 +18,7 @@ public class ServicioGeocercasApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("**************************************");
-		System.out.println("GEOCERCAS V1.1.2 24-11-05 16:11");
+		System.out.println("GEOCERCAS V1.1.4 24-12-24 13:33");
 	}
 
 }
